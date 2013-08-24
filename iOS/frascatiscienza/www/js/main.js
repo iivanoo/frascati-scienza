c869c0702b08972421a48988fec5560dbdc2ebda
ivanomalavolta@gmail.com <ivanomalavolta@gmail.com@25488cf3-2eb6-1e49-65c8-c6d6839316a7>
2013-08-24 10:53:14 +0000

    aggiunti gli spinner di caricamento. Un po di rifiniture grafiche :D

--- iOS/frascatiscienza/www/js/main.js
+++ iOS/frascatiscienza/www/js/main.js
@@ -48,8 +48,8 @@ var isEmpty = function(obj) {
       };
 
 // We launch the App
-require(['underscore', 'backbone', 'router', 'datamanager'],
-    function (_, Backbone, AppRouter, Data) {
+require(['underscore', 'backbone', 'spin', 'router', 'datamanager'],
+    function (_, Backbone, Spinner, AppRouter, Data) {
 
       String.prototype.endsWith = function(suffix) {
           return this.indexOf(suffix, this.length - suffix.length) !== -1;
@@ -62,6 +62,26 @@ require(['underscore', 'backbone', 'router', 'datamanager'],
       document.addEventListener("deviceready", run, false);
 
       function run() {
+        var opts = {
+          lines: 5, // The number of lines to draw
+          length: 15, // The length of each line
+          width: 5, // The line thickness
+          radius: 15, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb
+          speed: 1, // Rounds per second
+          trail: 60, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: '150px', // Top position relative to parent in px
+          left: 'auto' // Left position relative to parent in px
+        };
+        var spinner = new Spinner(opts);
+        setTimeout(function(){spinner.spin(document.getElementsByTagName("body")[0]);}, 20);
         // TODO check in che ordine andiamo a prenderci i dati e quando facciamo le query nel DB locale
         Data.initialize();
         var router = new AppRouter();
@@ -83,6 +103,7 @@ require(['underscore', 'backbone', 'router', 'datamanager'],
         }*/
 
         Data.on("dataReady", function() {
+          spinner.stop();
           Backbone.history.start();
         });
       }
