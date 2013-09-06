d48ac0f3b781850cb1b1ac8b40e5eb1526a4c3b6
ivanomalavolta@gmail.com <ivanomalavolta@gmail.com@25488cf3-2eb6-1e49-65c8-c6d6839316a7>
2013-09-06 19:57:05 +0000

    SOTTOMESSO ANDROID

--- Android/frascatiscienza/src/it/univaq/frascatiscienza/frascatiscienza.java
+++ Android/frascatiscienza/src/it/univaq/frascatiscienza/frascatiscienza.java
@@ -27,7 +27,7 @@ public class frascatiscienza extends DroidGap
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);  
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
