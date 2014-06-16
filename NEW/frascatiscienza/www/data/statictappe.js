9abb85fa00e56c5aa07f9503a3f5f5ff8d872a25
ivanomalavolta@gmail.com <ivanomalavolta@gmail.com@25488cf3-2eb6-1e49-65c8-c6d6839316a7>
2014-06-16 12:48:36 +0000

    Aggiornato con i sorgenti di Frascati Scienza vecchi 2

---
+++ NEW/frascatiscienza/www/data/statictappe.js
@@ -0,0 +1,119 @@
+define({
+  tappe: [{ 
+    id: "tappa1234",
+    titolo: "Tappa 1 – Farmacia del Gambero",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-1.mp4",
+    intro: ["La Farmacia del Gambero in Via della Vite è una delle più antiche di Roma. Custodisce un archivio che risale al 1672 con l'inventario di medicinali e dotazioni dell'antica \"Aphoteca ad usum semplicitarie\".", 
+        "A Roma nacque l'Università degli Aromatari, l'istituzione deputata a dettare norme e regole per esercitare l'attività di speziale. La sua origine si fa risalire all'8 marzo 1429 con la bolla di Martino V. Numerosissime erano le ricette per i medicamenti, alcune delle quali a metà tra scienza e superstizione, come la Theriaca, una panacea per tutti i mali composta da decine di ingredienti tra cui la carne essiccata di vipera.",
+        "Bisogna aspettare l'800 perchè la chimica fornisca le basi per sintetizzare composti terapeutici. I farmacisti per secoli furono i primi artefici del farmaco, divenendo, quindi, anche i pionieri della chimica farmacologica."
+    ],
+    domanda: "Come si chiamava la prima università per \"farmacisti\"?",
+    risposte: ["Università di Farmacologia", "Gilda degli Alchimisti", "Università degli Aromatari", "Università degli Speziali"],
+    rispostacorretta: "2",
+    indicazione: "Complimenti, ottima risposta, raggiungi Samorì a Sant’Ignazio",
+    luogo: {lat: "41.899245", lon: "12.47965"}
+  },
+  { 
+    id: "tappa2789",
+    titolo: "Tappa 2 – Chiesa di Sant’Ignazio",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-2.mp4",
+    intro: ["Il Collegio Romano era il principale seminario gesuita romano. Fondato da Ignazio di Loyola nel 1551. Nel 1787 Giuseppe Calandrelli  qui fondò  l'Osservatorio Astronomico.", 
+        "Proprio al di sopra della chiesa di Sant\'Ignazio, sulla sommità dei pilastri che avrebbero dovuto sostenere la cupola della chiesa, Padre Angelo Secchi, fa erigere la cupola osservativa per ospitare i telescopi. Gesuita, fisico ed astronomo, è nominato direttore dell’Osservatorio dopo pochi mesi dalla fine dei moti del 1848 a Roma. Con le osservazioni realizzate con questi strumenti diviene uno dei più importanti Astrofisici europei del 1800. Rendendo visibili le \"impronte digitali\" degli elementi chimici che costituiscono circa 4000 stelle, riesce a comprendere che esiste un numero finito di classi di stelle e che queste differiscono tra loro \"per l’ atmosfera e la temperatura\".",
+        "Tra il XVIII e il XIX secolo l\'astronomia, oltre allo studio dei corpi celesti, ricopriva anche alcuni compiti di pubblica utilità. Da Sant\'Ignazio ad esempio veniva dato il segnale per lo sparo del cannone che, da Castel Sant\'Angelo prima, e poi dal Gianicolo, scandiva il mezzogiorno a Roma.",
+        "Gli astronomi del Collegio Romano inviavano il segnale ai militari sul tetto di Castel Sant\'Angelo tramite una cesta di vimini nera, che veniva lasciata scorrere lungo un palo nel momento preciso in cui il sole segnava le 12 su una meridiana." 
+    ],
+    domanda: "Quale tradizione romana legava il Collegio Romano alla Città?",
+    risposte: ["Dal Collegio partivano i primi rintocchi di campana dopo l’alba", "Dal Collegio veniva dato il segnale per il colpo di cannone che segnava le 12 a Roma", "Nell’orto del Collegio si coltivavano i carciofi","Sul tetto della Chiesa di Sant’Ignazio era posta la statua della Lupa Capitolina"],
+    rispostacorretta: "1",
+    indicazione: "Hai indovinato. Segui le orme di Galileo a Piazza della Minerva",
+    luogo: {lat: "41.897831", lon: "12.477357"}
+  },
+  { 
+    id: "tappa3109",
+    titolo: "Tappa 3 – Piazza Minerva",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-3.mp4",
+    intro: ["22 giugno 1633 Galileo Galilei nel convento di Santa Maria sopra Minerva, al termine del processo che lo vide imputato di fronte l'Inquisizione nel 1633, firma l’Abiura.",
+        "Perchè Galileo abiura? Brecht riporta di una lettera in cui traspare il lato umano di uno scienziato che cede per paura delle torture, e abiura. Molti invece, ritengono che Galileo fece questa scelta per portare a compimento la sua ultima opera, quella che lascia un'eredità senza prezzo ai futuri scienziati e ricercatori: Discorsi e dimostrazioni matematiche intorno a due nuove scienze.",
+        "Metodo Scientifico. Forse il più grande contributo di Galileo al progresso scientifico è la formalizzazione del metodo sperimentale. Lo strumento per eccellenza per investigare la natura e cercare di comprendere il Perchè avvengono determinati fenomeni.",
+        "Questo metodo si contrappone all’ osservazione passiva di un fenomeno ed a spiegazioni acritiche o legate alla pura immaginazione. Si basa sull’ utilizzo di esperimenti riproducibili e indipendenti dall’osservatore o dal luogo dove si realizzano, e la registrazione di dati misurabili e oggettivi.",
+        "Questo è il metodo per comprendere la natura, ma, in fin dei conti, vale anche per qualunque situazione la vita quotidiana ci proponga."
+    ],
+    domanda: "In che anno abiurò Galileo?",
+    risposte: ["1609", "1633", "1632","1732"],
+    rispostacorretta: "1",
+    indicazione: "Risposta corretta. Vai a Piazza del Collegio Romano",
+    luogo: {lat: "41.898003", lon: "12.480388"}
+  },
+  { 
+    id: "tappa4961",
+    titolo: "Tappa 4 – Museo Kircheriano Piazza del Collegio romano",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-4.mp4",
+    intro: ["Elena Freda si laurea in matematica con Guido Castelnuovo nel 1912 e in fisica con Orso Mario Corbino nel 1915 all\'Università di Roma “La Sapienza”. Dopo aver insegnato a Roma ed essere stata a Messina per un breve periodo, a causa della mancanza di prospettive solide per la sua carriera, torna a Roma nel \'24 per proseguire i suoi studi e l\'insegnamento.",
+        "Assistente di Volterra già appena laureata, condivide molti dei lavori e delle teorie del famoso matematico. L\'utilizzo della razionalizzazione matematica in discipline come la biologia è solo uno degli esempi.",
+        "Il Museo Kircheriano, come era comunemente chiamato il Museo del Collegio Romano, nasce nel 1651. Rappresenta l\'archetipo dei moderni musei delle scienze. Deve la sua fama a Papa Alessandro VII che voleva rilanciare Roma come centro culturale, della fede e dello sviluppo scientifico. Roma doveva tornare ad essere Teatro del Mondo.",
+        "Il museo prende il nome dal direttore Padre Athanasius Kircher (1601/2-1680) che ne era il curatore ed il \"regista\".",  
+        "Nel museo si potevano trovare: opere d'arte, reperti antichi, obelischi egiziani, animali impagliati e non mancavano reperti e macchinari più che stravaganti: statue che parlavano, una coda di Sirena, manichini volanti sospesi nell\'aria."
+    ],
+    domanda: "Quale strano reperto si poteva vedere all’interno del Museo Kircheriano?",
+    risposte: ["I denti di una sirena", "Il tridente di Nettuno", "La coda di una sirena","Una statua di Padre Kircher"],
+    rispostacorretta: "2",
+    indicazione: "Bravo. Segui le indicazioni di Elena e cerca di raggiungere Ottolenghi a Palazzo Valentini",
+    luogo: {lat: "41.896887", lon: "12.483582"}
+  },
+  { 
+    id: "tappa5000",
+    titolo: "Tappa 5 – Palazzo Valentini",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-5.mp4",
+    intro: ["Tra il 2006 ed il 2010 è stata restaurata la domus romana di età imperiale, ca IV secolo d. C., posta sotto Palazzo Valentini, appartenente a una potente famiglia romana dell5\'epoca.",
+        "Oggi è visitabile una ricostruzione multimediale che offre la vista a mosaici, pareti decorate, pavimenti in marmo, statue, giardini e le terme che erano parte di una struttura di alcune migliaia di metri quadrati.", 
+    ],
+    domanda: "Di che epoca è la Domus Romana trovata sotto Palazzo Valentini?",
+    risposte: ["IV sec d. C.", "XV sec d. C.", "III sec  a. C.","V sec a. C."],
+    rispostacorretta: "0",
+    indicazione: "Vai a Via Panisperna per incontrare Ottolenghi in uno dei laboratori con cui collaborava",
+    luogo: {lat: "41.896727", lon: "12.492333"}
+  },
+  { 
+    id: "tappa6143",
+    titolo: "Tappa 6 – Via Panisperna",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-6.mp4",
+    intro: ["Ottolenghi nasce nel 1861 ad Asti e si laurea in medicina e chirurgia a Torino a 23 anni. Allievo di Cesare Lombroso, creatore della disciplina della criminologia scientifica, studia inoltre Antropologia e Psichiatria. Nel 1902 nasce la Scuola Superiore di Polizia, uno dei suoi maggiori traguardi. Arriva a Roma nel 1903 dove prosegue il suo lavoro nella stesura di un metodo scientifico applicato alle indagini di polizia. Qui formalizza il \"Programma di Polizia Scientifica\" nel quale psicologia, medicina legale, antropologia e psichiatria trovavano applicazione concreta nel settore delle investigazioni, fino a quel momento lasciate \"all’intuito del funzionario di polizia\".", 
+        "Il 20 settembre 1870 avviene la presa di Roma, che l’anno dopo diverrà capitale. Il ministro Quintino Sella fa suo il concetto espresso da Mazzini e Garibaldi della Terza Roma, una capitale faro culturale e scientifico per il mondo. Si impegna per costruire uno Stato Italiano che \"chieda molto ai suoi cittadini, ma che sia capace di restituire anche molto\". Gli anni dell'unificazione non sono anni facili per l'economia, ma, qualunque taglio o imposta, non va mai ad intaccare l'investimento nell'Istruzione, la formazione e lo sviluppo delle scienze.",
+        "Nel giugno del 1872 fa promulgare un disegno di legge per il riordino delle Istituzioni Scientifiche e avvia la realizzazione di un Polo delle Scienze per ospitare gli Istituti di Chimica, Fisica e Matematica, a Via Panisperna 89/a, incaricando i migliori esponenti di queste discipline: il chimico Stanislao Cannizzaro, il fisico Pietro Blaserna ed il matematico Luigi Cremona."
+    ],
+    domanda: "Cosa sorgeva a via Panisperna 89/a?",
+    risposte: ["L’Istituto di Frenologia", "Il Polo scientifico di Chimica, Fisica e Matematica", "L’Istituto di Polizia Scientifica","Il Polo di ricerca della Polizia"],
+    rispostacorretta: "1",
+    indicazione: "Complimenti, hai risposto correttamente. Segui Samorì alla Biblioteca Casanatense per raccogliere altri indizi",
+    luogo: {lat: "41.898823", lon: "12.479459"}
+  },
+  { 
+    id: "tappa7263",
+    titolo: "Tappa 7 – Biblioteca Casanatense",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-7.mp4",
+    intro: ["Margarethe Traube Mengarini nasce a Berlino nel 1856 in una famiglia di scienziati ed intellettuali.",
+        "Fervente emancipazionista, è una delle prime due donne laureatesi in Scienze a Roma, nel 1883. Il suo spirito di iniziativa nell\'affrontare la cultura e la scienza, la portano a collaborare in vari laboratori, tra Roma, Napoli e Berlino.",
+        "Nella sua residenza vicino al Quirinale, istituì un circolo culturale che vantava la presenza di personaggi di spicco del mondo scientifico e culturale, Romano ed Europeo.",
+        "La biblioteca deve l'origine e il nome al card. Girolamo Casanate che, nel 1698, destinò la sua raccolta di 25.000 volumi e la somma di 160.000 scudi alla fondazione d'una grande biblioteca pubblica a Roma.",
+        "Tra i tesori conservati: un esemplare di \"Exultet\", rotolo miniato del medioevo meridionale (sec.XII), un gruppo di manoscritti appartenenti all'Ordine dei Templari, codici di argomento medico dei secoli XIII e XIV tra cui la Chirurgia attribuita tra il 1210 e il 1250 al maestro Rolando della Scuola Salernitana di medicina,  il Theatrum sanitatis, vero compendio di scienza naturale con le descrizioni di piante, animali e minerali e riferimenti alle loro virtù mediche e terapeutiche un\' enciclopedia medica del sec. XIV realizzato per il re Venceslao di Lussemburgo."
+    ],
+    domanda: "Come si chiama l\'enciclopedia medica realizzata per il re Venceslao conservata alla Biblioteca Casanatense?",
+    risposte: ["Exultet", "Trattum sanitatis", "Theatrum sanitatis","Chirurgia"],
+    rispostacorretta: "2",
+    indicazione: "Sei vicino a risolvere il mistero. Raggiungi Piazza Colonna e scoprirai la soluzione dell’enigma!",
+    luogo: {lat: "41.900806", lon: "12.480349"}
+  },
+  { 
+    id: "tappa8123",
+    titolo: "Tappa 8 – Piazza Colonna",
+    video: "http://www.frascatiscienza.it/wp-content/uploads/2013/08/scena-8.mp4",
+    intro: ["Vito Volterra nasce nel 1860 con un talento innato per la matematica. A soli 13 anni, dopo aver letto Dalla Terra alla Luna di Verne, calcola la traiettoria balistica di una navicella, sottoposta all\'attrazione gravitazionale della Terra e della Luna.",
+        "Prima di giungere a Roma nel 1900, insegna a Torino e sviluppa una fitta rete di contatti internazionali. In quegli anni mette le basi per quello che diventerà il suo soprannome in tutte le università europee: \"Mr Scienza Italiana\".", 
+        "Diverrà famoso per i suoi lavori sull\'applicazione della Matematica alla Biologia, all'Economia e ad altre scienze \"meno teoriche\". Nel 1903 diviene senatore e inizia a lavorare alla costruzione di nuove Istituzioni Scientifiche Italiane sul modello europeo. Dopo la fine della Prima Guerra Mondiale avvia il percorso che porterà, nel 1923,  alla nascita del CNR, di cui sarà il primo presidente.",
+        "Durante una visita in Germania gli fu chiesto come mai gli scienziati, pur non conoscendosi e provenendo da paesi e culture diverse, si sentano subito amici tra loro. Rispose: \"Perchè hanno un amico in comune: la Scienza!\"",
+    ],
+    finale: true
+  }
+  ]
+});
+
