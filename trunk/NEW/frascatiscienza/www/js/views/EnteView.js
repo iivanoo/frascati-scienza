f9c0c882fa1458767f3523acbb2f1eb371e38fcc
ivanomalavolta@gmail.com <ivanomalavolta@gmail.com@25488cf3-2eb6-1e49-65c8-c6d6839316a7>
2014-09-08 17:11:16 +0000

    test per Alessio Android

--- trunk/NEW/frascatiscienza/www/js/views/EnteView.js
+++ trunk/NEW/frascatiscienza/www/js/views/EnteView.js
@@ -16,6 +16,7 @@ define(["jquery", "underscore", "backbone", "models/Ente", "handlebars", "text!t
         className: "default_wrapper",
 
         initialize: function() {
+            alert(2);
             this.title = this.model.get("titolo");
             this.moving = false;
             this.on("inTheDom", function(e) {
